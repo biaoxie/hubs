a87514af3942a1bf7e6fe498ed6e77fd75dec73c
Manuel Martin <manmartin@mozilla.com>
2022-03-16 17:25:27 +0100

    Consolidate video and speaker options

--- src/react-components/preferences-screen.js
+++ src/react-components/preferences-screen.js
@@ -837,7 +837,7 @@ class PreferencesScreen extends Component {
     const preferredMic = { ...this.state.preferredMic };
     preferredMic.options = micOptions;
 
-    const speakersOptions = this.mediaDevicesManager.outputDevices.map(device => ({
+    const speakersOptions = this.mediaDevicesManager.outputDevicesOptions.map(device => ({
       value: device.value,
       text: device.label
     }));
@@ -845,7 +845,7 @@ class PreferencesScreen extends Component {
     preferredSpeakers.options = speakersOptions?.length > 0 ? speakersOptions : [{ value: "none", text: "None" }];
 
     // Video devices update
-    const videoOptions = this.mediaDevicesManager.videoDevices.map(device => ({
+    const videoOptions = this.mediaDevicesManager.videoDevicesOptions.map(device => ({
       value: device.value,
       text: device.label
     }));
